c3a7bd774f260236a8f51decbd01a06e97ea1eff
zyxiaohuoji <1275819010@qq.com>
2020-12-23 15:50:52 +0800

    工资套账添加

--- src/views/sal/SalSob.vue
+++ src/views/sal/SalSob.vue
@@ -1,12 +1,288 @@
 <template>
   <div>
-    工资账套管理
+    <div style="display: flex; justify-content: space-between">
+      <el-button icon="el-icon-plus" type="primary" @click="showAddSalaryView">添加工资帐套</el-button>
+      <el-button icon="el-icon-refresh" type="success" @click="initSalaries"></el-button>
+    </div>
+    <div style="margin-top: 15px;">
+      <el-table
+          :data="salaries"
+          border
+          stripe
+          style="width: 100%">
+        <el-table-column
+            width="50px"
+            align="center"
+            type="selection">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="帐套名称"
+            align="center"
+            width="120">
+        </el-table-column>
+        <el-table-column
+            prop="basicSalary"
+            label="基本工资"
+            align="center"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="trafficSalary"
+            label="交通补助"
+            align="center"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="lunchSalary"
+            label="午餐补助"
+            align="center"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="bonus"
+            label="奖金"
+            align="center"
+            width="70">
+        </el-table-column>
+        <el-table-column
+            prop="createDate"
+            label="启用时间"
+            align="center"
+            width="80">
+        </el-table-column>
+
+        <el-table-column
+            label="养老金"
+            align="center">
+          <el-table-column
+              prop="pensionPer"
+              label="比率"
+              align="center"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="pensionBase"
+              label="基数"
+              align="center"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+        <el-table-column
+            label="医疗保险"
+            align="center">
+          <el-table-column
+              prop="medicalPer"
+              label="比率"
+              align="center"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="medicalBase"
+              label="基数"
+              align="center"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+
+        <el-table-column
+            label="公积金"
+            align="center">
+          <el-table-column
+              prop="accumulationFundPer"
+              align="center"
+              label="比率"
+              width="70">
+          </el-table-column>
+          <el-table-column
+              prop="accumulationFundBase"
+              align="center"
+              label="基数"
+              width="70">
+          </el-table-column>
+        </el-table-column>
+        <el-table-column align="center"
+                         fixed="right"
+                         label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" style="padding: 5px;" @click="showEditSalaryView(scope.row)">编辑</el-button>
+            <el-button size="mini" style="padding: 5px;" type="danger" @click="deleteSalary(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </div>
+    <el-dialog
+        :title="dialogTitle"
+        :visible.sync="dialogVisible"
+        width="550px">
+      <div style="display: flex; justify-content: space-around; align-items: center">
+        <el-steps direction="vertical" :active="activeItemIndex">
+          <el-step :title="itemName"
+                   v-for="(itemName, index) in salaryItemName"
+                   :key="index">
+
+          </el-step>
+        </el-steps>
+        <el-input v-model="salary[title]" :placeholder="'请输入' + salaryItemName[index] + '……'"
+                  v-for="(value, title, index) in salary"
+                  v-show="activeItemIndex == index"
+                  :key="index" style="width: 200px;">
+        </el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="preStep">{{activeItemIndex == 10 ? '取消' : '上一步'}}</el-button>
+    <el-button type="primary" @click="nextStep">{{activeItemIndex == 10 ? '完成' : '下一步'}}</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SalSob"
+  name: "SalSob",
+
+  data(){
+    return{
+      dialogTitle: "添加员工套账",
+      salaries: [],
+      dialogVisible: false,
+      activeItemIndex: 0,
+      salaryItemName: [
+        '基本工资',
+        '交通补助',
+        '午餐补助',
+        '奖金 ',
+        '养老金比率',
+        '养老金基数',
+        '医疗保险比率',
+        '医疗保险基数',
+        '公积金比率',
+        '公积金基数',
+        '帐套名称'
+      ],
+      salary: {
+        basicSalary: 0,
+        trafficSalary: 0,
+        lunchSalary: 0,
+        bonus: 0,
+        pensionPer: 0.0,
+        pensionBase: 0,
+        medicalPer: 0.0,
+        medicalBase: 0,
+        accumulationFundPer: 0.0,
+        accumulationFundBase: 0,
+        name: ''
+      }
+    }
+  },
+
+  mounted() {
+    this.initSalaries();
+  },
+
+  methods:{
+
+    deleteSalary(data){
+      this.$confirm('此操作将永久删除【' + data.name + '】, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest("/salary/sob/" + data.id).then(resp=>{
+          if (resp) {
+            this.initSalaries();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+
+    preStep() {
+      if (this.activeItemIndex == 0){
+        return ;
+      } else if (this.activeItemIndex == 10) {
+      //  数据输出
+      //  关闭对话框
+        this.dialogVisible = false;
+        this.activeItemIndex = 0;
+        return ;
+      }
+      this.activeItemIndex--;
+    },
+
+    nextStep() {
+      if (this.activeItemIndex == 10){
+        if (this.salary.id) {
+          this.putRequest("/salary/sob/", this.salary).then(resp=>{
+            if (resp){
+              this.initSalaries();
+              this.dialogVisible = false;
+            }
+          })
+        } else {
+          this.postRequest("/salary/sob/", this.salary).then(resp=>{
+            if (resp){
+              this.initSalaries();
+              this.dialogVisible = false;
+            }
+          })
+        }
+        return ;
+      }
+      this.activeItemIndex++;
+    },
+
+    showAddSalaryView() {
+      //数据初始化
+      this.salary = {
+        basicSalary: 0,
+        trafficSalary: 0,
+        lunchSalary: 0,
+        bonus: 0,
+        pensionPer: 0,
+        pensionBase: 0,
+        medicalPer: 0,
+        medicalBase: 0,
+        accumulationFundPer: 0,
+        accumulationFundBase: 0,
+        name: '默认名称'
+      }
+      this.dialogTitle = "添加工资套账";
+      this.dialogVisible = true;
+
+    },
+
+    showEditSalaryView(data) {
+
+      this.dialogTitle = '修改工资账套';
+      this.dialogVisible = true;
+      this.salary.basicSalary = data.basicSalary;
+      this.salary.trafficSalary = data.trafficSalary;
+      this.salary.lunchSalary = data.lunchSalary;
+      this.salary.bonus = data.bonus;
+      this.salary.pensionPer = data.pensionPer;
+      this.salary.pensionBase = data.pensionBase;
+      this.salary.medicalPer = data.medicalPer;
+      this.salary.medicalBase = data.medicalBase;
+      this.salary.accumulationFundPer = data.accumulationFundPer;
+      this.salary.accumulationFundBase = data.accumulationFundBase;
+      this.salary.name = data.name;
+      this.salary.id = data.id;
+    },
+
+    initSalaries() {
+      this.getRequest("/salary/sob/").then(resp=>{
+        if (resp) {
+          this.salaries = resp;
+        }
+      })
+    }
+  }
 }
 </script>
 
